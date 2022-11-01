6b95c10e570c18b407de93cf12821ab989c549d3
zargovv <zargovv@yandex.ru>
2022-11-02 00:12:40 +0600

    feat: move to npm-dts-webpack-plugin for declaration file generation

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
-const BundleDeclarationsWebpackPlugin = require("bundle-declarations-webpack-plugin");
+const NpmDtsWebpackPlugin = require("npm-dts-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const nodeExternals = require("webpack-node-externals");
 
+/** @type {import("webpack").Configuration} */
 module.exports = {
   entry: "./lib/index.ts",
   target: "node",
@@ -10,24 +11,12 @@ module.exports = {
     rules: [{ test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ }]
   },
   plugins: [
-    new BundleDeclarationsWebpackPlugin({
-      entry: "./lib/index.ts",
-      outFile: "index.d.ts"
-    }),
+    new NpmDtsWebpackPlugin({ output: "./bundle/index.d.ts" }),
     new CopyWebpackPlugin({
       patterns: [{ from: "target/release/gif_ffi.dll", to: "." }]
     })
   ],
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-    // fallback: {
-    //   os: false,
-    //   path: false,
-    //   buffer: false,
-    //   util: false,
-    //   assert: false
-    // }
-  },
+  resolve: { extensions: [".tsx", ".ts", ".js"] },
   output: {
     filename: "index.js",
     libraryTarget: "umd",
